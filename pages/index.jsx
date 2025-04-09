2eb6bb7fb058dfcdecdc6b02422f50fde3944844
Anastasia-hash-collab <ryago.nastya71@gmail.com>
2025-04-09 15:16:18 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,11 +5,11 @@ export default function Home() {
     <div style={{ minHeight: '100vh', fontFamily: 'sans-serif' }}>
       {/* Шапка с логотипом */}
       <header style={{ display: 'flex', alignItems: 'center', padding: '10px 20px' }}>
-        <img src="/логотип.png" alt="Логотип МонАрх" style={{ height: '50px' }} />
+        <img src="/логотип.png" alt="Логотип МонАрх" style={{ height: '100px' }} />
       </header>
 
       {/* Основной контент */}
-      <main style={{ padding: '20px' }}>
+      <main style={{ padding: '10px' }}>
         <h1>Добро пожаловать на калькулятор монтажа!</h1>
         <p>Здесь будет ваш основной функционал...</p>
       </main>
